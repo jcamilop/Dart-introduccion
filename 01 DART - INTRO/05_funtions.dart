c1e03bc8389c5ce08836eb7cdb2edcd9e32e7fc3
juan camilo palacios <juank01102114@gmail.com>
2025-09-10 10:54:21 -0500

    funtions and parameter

--- 01 DART - INTRO/05_funtions.dart
+++ 01 DART - INTRO/05_funtions.dart
@@ -1,16 +1,17 @@
 void main() {
  print(helloworld());
  print(addhello());
- print('la suma es: ${hellonumbers(1, 5)}');
-  print('la suma es: ${addnumbers(1, 5)}');
+ print('the sum is: ${hellonumbers(1, 5)}');  
+ print('the sum is: ${addnumbers(1, 5)}');  
+ print(data(name: 'camilo'));
   }
 
  String helloworld(){               //funcion de con return
-  return 'hola a todos';
+  return 'hello everyone';
   
 }
 
-String addhello() => 'hola a todos';      //return de tipo flecha
+String addhello() => 'hello everyone';      //return de tipo flecha
 
 int hellonumbers(int a, int b){
   return (a + b);
@@ -18,3 +19,9 @@ int hellonumbers(int a, int b){
 }
 
 int addnumbers(int a, int b) => a + b;
+
+int addnumbers(int a, int b) => a + b;
+
+String data({required String name, int age = 25}){
+  return 'hello sr $name his age is $age' ;
+}
